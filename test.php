892865cdbd51d37d1ce0199040fd18d84c74e2a6
Alex <abjelosevic@ogosense.com>
2014-02-18 08:58:36 -0500

    added php script

--- test.php
+++ test.php
@@ -1,5 +1,23 @@
 <?php 
 	
-echo "Hello World!";
+$mysql_server = "";
+$mysql_user = "";
+$mysql_password = "";
+$mysql_db = "";
+$con = mysqli_connect($mysql_server, $mysql_user, $mysql_password, $mysql_db);
+if (mysqli_connect_errno($con)) 
+{
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	exit();
+}
+else
+{
+	$query = "SELECT * FROM `table_name`";
+	$result = mysqli_query($con, $query);
+	while($row = mysqli_fetch_array($result))
+	{
+		echo "Hello World!";
+	}
+}
 
 ?>
